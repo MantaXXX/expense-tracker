65f6e9f3fb8429372fb9a30006814bed3a6a06c8
Jia <pochialu@gmail.com>
2020-08-10 01:56:33 +0800

    feat: add mongoDB and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const PORT = 3000
+
+mongoose.connect('mongodb://localhost/expenseTracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('error');
+})
+db.once('open', () => {
+  console.log('mongoDB connected!');
+})
+
 
 const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -12,8 +24,15 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+// New
+app.get('/new', (req, res) => {
+  res.render('new')
+})
 
+app.post('/new', (req, res) => {
+
+})
 
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost${PORT}`);
+  console.log(`App is running on http://localhost:${PORT}`);
 })
