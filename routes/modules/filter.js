30d59049e9a43dd14c681ca64ad5ee3c2bfa14dc
Jia <pochialu@gmail.com>
2020-08-27 14:02:07 +0800

    feat: modify filter.js for displaying all

--- routes/modules/filter.js
+++ routes/modules/filter.js
@@ -13,14 +13,15 @@ router.get('/', (req, res) => {
         .sort({ date: 'asc' })
         .then(records => {
           const category = req.query.category
-          // if (category === '全部') {
-          //   const totalAmount = records.map(filteredRecord => filteredRecord.amount).reduce((a, b) => { return a + b }, 0)
-          //   return res.render('index', { records, totalAmount })
-          // } else {
-          const filteredRecords = records.filter(filterRecord => filterRecord.category === category)
-          const totalAmount = records.map(filteredRecord => filteredRecord.amount).reduce((a, b) => { return a + b }, 0)
-          res.render('index', { records: filteredRecords, totalAmount, category })
-          // }
+          console.log(category)
+          if (category === '全部') {
+            const totalAmount = records.map(filteredRecord => filteredRecord.amount).reduce((a, b) => { return a + b }, 0)
+            return res.render('index', { records, totalAmount })
+          } else {
+            const filteredRecords = records.filter(filterRecord => filterRecord.category === category)
+            const totalAmount = records.map(filteredRecord => filteredRecord.amount).reduce((a, b) => { return a + b }, 0)
+            res.render('index', { records: filteredRecords, totalAmount, category })
+          }
         })
         .catch(error => console.log(error))
     })
